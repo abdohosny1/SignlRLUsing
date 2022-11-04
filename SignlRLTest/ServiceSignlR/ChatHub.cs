908e211103158fccb8b80323204237fcd19b9b4e
abdohosny1 <make.y90@yahoo.com>
2022-11-04 15:19:23 +0200

    Add project files.

---
+++ SignlRLTest/ServiceSignlR/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignlRLTest.ServiceSignlR
+{
+    public class ChatHub : Hub
+    {
+       
+            public void SendMessage(string name , string message)
+            {
+            // dbsave
+
+            Clients.All().newMessage(name,message); //send all
+            Clients.Others(); // send all but not send to resender
+            }
+        
+    }
+}
---
+++ SignlRLTest/Startup1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignlRLTest.Startup1))]
+
+namespace SignlRLTest
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //first step
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
